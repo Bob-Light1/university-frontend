2d1ba45a8af0c75022f71ae7b748f2825579f8f7
Bob Light <boblagno6@gmail.com>
2026-05-27 03:01:40 +0100

    Detail Drawer appearence for partners.

--- src/campus/components/partners/PartnerDetailDrawer.jsx
+++ src/campus/components/partners/PartnerDetailDrawer.jsx
@@ -179,7 +179,7 @@ const PartnerDetailDrawer = ({
           background: BRAND_GRADIENT,
           color: 'white',
           position: 'relative',
-          marginTop: 5,
+          marginTop: 7,
         }}
       >
         <IconButton
